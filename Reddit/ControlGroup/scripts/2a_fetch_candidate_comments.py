2dc02c7381553ae3c5c0494ebfe216f5ee6108af
Hannah Lybbert <hannah.lybbert@chicagobooth.edu>
2026-09-03 09:57:02 -0500

    Create fetch comments and submissions scripts for candidate pool

---
+++ Reddit/ControlGroup/scripts/2a_fetch_candidate_comments.py
@@ -0,0 +1,4 @@
+# Author: Hannah Lybbert
+# Created: 2026-09-02
+# Purpose: For each candidate user from ControlGroup/data/1_candidate_pool.parquet, fetch their comments from the raw comments data
+# Output: One file per author with all of their comments saved to Reddit/ControlGroup/data/per_author_candidates/{author name}_comments.parquet
---
+++ Reddit/ControlGroup/scripts/2b_fetch_candidate_submissions.py
@@ -0,0 +1,4 @@
+# Author: Hannah Lybbert
+# Created: 2026-09-02
+# Purpose: For each candidate user from ControlGroup/data/1_candidate_pool.parquet, fetch their submissions from the raw submissions data
+# Output: One file per author with all of their submissions saved to Reddit/ControlGroup/data/per_author_candidates/{author name}_submissions.parquet
